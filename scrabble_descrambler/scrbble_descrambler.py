226c513607e846e899c1856d69b734d8d7cac891
mpaydar <m21newyork@gmail.com>
2023-06-29 11:14:20 -0400

    Sorting Algorithm -signature-updated features

--- scrabble_descrambler/scrbble_descrambler.py
+++ scrabble_descrambler/scrbble_descrambler.py
@@ -1,6 +1,8 @@
 import signature
 
 word_directory = {}
+
+
 def read_file():
     file_letters = []
     with open("wordlist.txt", 'r') as file:
@@ -39,23 +41,49 @@ def directory_populating(tuple_list):
     return word_directory
 
 
+def filter_dictionary(words_director):
+    word_signature = words_director.items()  # each item is (signature,values)
+    temp_list = []
+    filtered_list = []
+    print("Number of words before filter: ",len(word_signature))
+    for words_tuple_index in word_signature:
+        temp_list.append(words_tuple_index)
+    for index in range(len(temp_list)):
+        if len(temp_list[index][0]) == 6:
+            filtered_list.append(temp_list[index])
+    print("Number of words after filter: ",len(filtered_list))
+    # print(temp_list[0:11])
+    # print(filtered_list[0:11])
+    return filtered_list
+
+
+def generate_dictionary(word_list):
+    filtered=dict()
+    for w_index in range(len(word_list)):
+        filtered[word_list[w_index][0]]=word_list[w_index][1]
+    return filtered
+
+
+
+
+
 def directory_lookup(user_input):
     return word_directory[user_input]
-
-
-
 
 
 original_voc = read_file()
 tuples = generate_tuple_list(original_voc)
 # print(tuples[2][0])  # signature element of the tuple    (0=signature,1=actual_word)
-d = directory_populating(tuples)
-# print(d)
+noFiltered_directory = directory_populating(tuples)
+filtered_tuples=filter_dictionary(noFiltered_directory)
+filtered_dictionary=generate_dictionary(filtered_tuples)
+# print(filtered_dictionary)
+user_input = input("Enter six letters (without any space) to see all the words containing these letters: ")
+user_input_lexOrder = signature.signature(user_input)
+# print(type(user_input_lexOrder))
 
-user_input=input("Enter six letters (without any space) to see all the words containing these letters: ")
-user_input_lexOrder=signature.signature(user_input)
-user_input_lexOrder=convert_letter(user_input_lexOrder)
-user_respond=directory_lookup(user_input_lexOrder)
+
+
+user_input_lexOrder = convert_letter(user_input_lexOrder)
+user_respond = directory_lookup(user_input_lexOrder)
 print("Here's the list of possible words you may use with the 6 letters you have provided: ", user_respond)
-
-
