9a378831689b4c2bda6da81135bd435b47f85ae9
mpaydar <m21newyork@gmail.com>
2023-06-29 09:58:17 -0400

    added comments on the signature file, for documentation purposes

--- scrabble_descrambler/signature.py
+++ scrabble_descrambler/signature.py
@@ -1,34 +1,41 @@
+
+# Function description:
+# Signature function follows an algorithm which sort each list that represent a word in alphabetic order:
+# Ascending order The algorithm will compare the current letter l, with all other letters
+# on its right side The character comparison is done all in terms of their ASCI code.
+
+# @parameters:
+# n: individual word; type: string
 def signature(n):
-    lex_order={}
-    letters=[]
-    old_letter=""
+    lex_order = {}      # holding the word n(the word) in this dictionary for the initial design of this function
+    letters = []       # holding all the character within the word n
+    old_letter = ""   # holding the character which is being compared to all other characters on its right hand side
     for l in range(len(n)):
-        letter=n[l]
-        lex_order[l]=letter
-    k=lex_order.values()
-    for letter in k:
+        letter = n[l]
+        lex_order[l] = letter  # indexes are the key ; letters are the key; Example: {0:'A'}
+    k = lex_order.values()     # Getting a dictionary object that include only the letters dic_val([A',...])
+    for letter in k:           # iterating through the dictionary object and putting the values in the array "letters"
         letters.append(letter)
+    # My sorting algorithm which sort each list that represent a word in alphabetic order: Ascending order
+        # The algorithm will compare the current letter l, with all other letters on its right side
+        # The character comparison is done all in terms of their ASCI code.
     for l in range(len(letters)):
-        next=l+1
-        old_letter+=letters[l]
-        for next_letter in range(next,len(letters)):
+        next = l + 1
+        old_letter += letters[l]
+        for next_letter in range(next, len(letters)):
+            # The swapping happening using character ASCI representation
+            # If the next_letter has lower ASCI than current letter l , they will be swapped in the letters array
             if letters[next_letter] < letters[l]:
-                temp=letters[l]
-                letters[l]=letters[next_letter]
-                letters[next_letter]=temp
+                temp = letters[l]
+                letters[l] = letters[next_letter]
+                letters[next_letter] = temp
+    # Edge case: if there was no swap , then the combination of all the old letters represent the same word n,
+        # hence the same word will be returned
     if old_letter == n:
         return n
 
     return letters
 
 
-
-
-
-
-
-
-
-# a=signature("STOP")
-# b=convert_letter(a)
-# print(b)
+# a = signature("STOP")
+# print(a)
